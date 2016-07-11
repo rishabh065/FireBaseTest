d13ecc150010ed82b4c10d12b518ee3c0b8d13c6
rishabh065 <rishabh.4397@gmail.com>
2016-07-11 16:54:56 +0530

    Maps Activity added

--- app/src/main/java/ircs/com/firstaid/Fragments/ThreeFragment.java
+++ app/src/main/java/ircs/com/firstaid/Fragments/ThreeFragment.java
@@ -5,45 +5,21 @@ package ircs.com.firstaid.Fragments;
  */
 
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.ArrayList;
-
-import ircs.com.firstaid.PlacesService;
+import ircs.com.firstaid.MapsActivity;
 import ircs.com.firstaid.R;
-import ircs.com.firstaid.place;
 
 
 public class ThreeFragment extends Fragment{
     private final String TAG = getClass().getSimpleName();
-    private GoogleMap mMap;
-    private LocationManager locationManager;
-    private Location loc;
-    MapView mapView;
-    SupportMapFragment mapFragment;
+
     public ThreeFragment() {
         // Required empty public constructor
     }
@@ -51,142 +27,21 @@ public class ThreeFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-            View view=inflater.inflate(R.layout.fragment_three, container, false);
-        mapView= (MapView) view.findViewById(R.id.map);
-        System.out.println("MapInit1");
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        FloatingActionButton fab = (FloatingActionButton)view. findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapReady(GoogleMap googleMap) {
-                System.out.println("MapInit2");
-                mMap=googleMap;
+            public void onClick(View v) {
+                Intent i=new Intent(getActivity(), MapsActivity.class);
+                getActivity().startActivity(i);
             }
         });
-        currentLocation();
         return view;
-    }
-
-
-    private class GetPlaces extends AsyncTask<Void,Void,ArrayList<place>> {
-
-        private ProgressDialog dialog;
-        private Context context;
-        private String places;
-
-        public GetPlaces(Context context, String places) {
-            this.context = context;
-            this.places = places;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<place> result) {
-            super.onPostExecute(result);
-            if (dialog.isShowing()) {
-                dialog.dismiss();
-            }
-            for (int i = 0; i < result.size(); i++) {
-                mMap.addMarker(new MarkerOptions()
-                        .title(result.get(i).getName())
-                        .position(
-                                new LatLng(result.get(i).getLatitude(), result
-                                        .get(i).getLongitude()))
-                        .snippet(result.get(i).getVicinity()));
-            }
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(result.get(0).getLatitude(), result
-                            .get(0).getLongitude())) // Sets the center of the map to
-                    // Mountain View
-                    .zoom(14) // Sets the zoom
-                    .tilt(30) // Sets the tilt of the camera to 30 degrees
-                    .build(); // Creates a CameraPosition from the builder
-            mMap.animateCamera(CameraUpdateFactory
-                    .newCameraPosition(cameraPosition));
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog = new ProgressDialog(context);
-            dialog.setCancelable(false);
-            dialog.setMessage("Loading..");
-            dialog.isIndeterminate();
-            dialog.show();
-        }
-
-        @Override
-        protected ArrayList<place> doInBackground(Void... arg0) {
-            PlacesService service = new PlacesService("AIzaSyBcKckh-rMxnH0KXmek_MygitkEvBSXpLo");
-            ArrayList<place> findPlaces = (ArrayList<place>) service.findPlaces(28.632808 // 28.632808
-                    ,77.218276); // 77.218276
-
-
-            for (int i = 0; i < findPlaces.size(); i++) {
-
-                place placeDetail = findPlaces.get(i);
-                Log.e(TAG, "places : " + placeDetail.getName());
-            }
-            return findPlaces;
-        }
-    }
-    private void currentLocation() {
-        locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        String provider = locationManager
-                .getBestProvider(new Criteria(), false);
-
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        if (location == null) {
-            locationManager.requestLocationUpdates(provider, 0, 0, listener);
-        } else {
-            loc = location;
-            new GetPlaces(getActivity(),
-                    "hospitals").execute();
-            Log.e(TAG, "location : " + location);
-        }
-
-    }
-
-    private LocationListener listener = new LocationListener() {
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-        }
-
-        @Override
-        public void onLocationChanged(Location location) {
-            Log.e(TAG, "location update : " + location);
-            loc = location;
-            locationManager.removeUpdates(listener);
-        }
-    };
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        mapView.onResume();
-//
-//    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
     }
 }
---
+++ app/src/main/java/ircs/com/firstaid/MapsActivity.java
@@ -0,0 +1,167 @@
+package ircs.com.firstaid;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private final String TAG = getClass().getSimpleName();
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Location loc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        currentLocation();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        new GetPlaces(MapsActivity.this,
+                "hospital").execute();
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    private class GetPlaces extends AsyncTask<Void,Void,ArrayList<place>> {
+
+        private ProgressDialog dialog;
+        private Context context;
+        private String places;
+
+        public GetPlaces(Context context, String places) {
+            this.context = context;
+            this.places = places;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<place> result) {
+            super.onPostExecute(result);
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+            for (int i = 0; i < result.size(); i++) {
+                mMap.addMarker(new MarkerOptions()
+                        .title(result.get(i).getName())
+                        .position(
+                                new LatLng(result.get(i).getLatitude(), result
+                                        .get(i).getLongitude()))
+                        .snippet(result.get(i).getVicinity()));
+            }
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(result.get(0).getLatitude(), result
+                            .get(0).getLongitude())) // Sets the center of the map to
+                    // Mountain View
+                    .zoom(14) // Sets the zoom
+                    .tilt(30) // Sets the tilt of the camera to 30 degrees
+                    .build(); // Creates a CameraPosition from the builder
+            mMap.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(cameraPosition));
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(context);
+            dialog.setCancelable(false);
+            dialog.setMessage("Loading..");
+            dialog.isIndeterminate();
+            dialog.show();
+        }
+
+        @Override
+        protected ArrayList<place> doInBackground(Void... arg0) {
+            PlacesService service = new PlacesService("AIzaSyBcKckh-rMxnH0KXmek_MygitkEvBSXpLo");
+            ArrayList<place> findPlaces = (ArrayList<place>) service.findPlaces(loc.getLatitude(), // 28.632808
+                    loc.getLongitude()); // 77.218276
+
+
+            for (int i = 0; i < findPlaces.size(); i++) {
+
+                place placeDetail = findPlaces.get(i);
+                Log.e(TAG, "places : " + placeDetail.getName());
+            }
+            return findPlaces;
+        }
+    }
+    private void currentLocation() {
+        System.out.println("INside1");
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        String provider = locationManager
+                .getBestProvider(new Criteria(), false);
+
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location == null) {
+            locationManager.requestLocationUpdates(provider, 0, 0, listener);
+            System.out.println("INside3");
+        } else {
+            System.out.println("INside2");
+            loc = location;
+            new GetPlaces(MapsActivity.this,
+                    "hospitals").execute();
+            Log.e(TAG, "location : " + location);
+        }
+
+    }
+
+    private LocationListener listener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.e(TAG, "location update : " + location);
+            loc = location;
+            locationManager.removeUpdates(listener);
+        }
+    };
+}
